65b821a935ea8b58cecf93eb95f6502536bce38f
tandingbo@163.com <tandingbo>
2016-02-04 16:55:47 +0800

    tanbobo 1.0.0

--- src/main/java/com/tanbobo/cms/core/controller/SysAccountController.java
+++ src/main/java/com/tanbobo/cms/core/controller/SysAccountController.java
@@ -3,6 +3,8 @@ package com.tanbobo.cms.core.controller;
 import com.tanbobo.cms.base.controller.BaseController;
 import com.tanbobo.cms.core.service.ISysAccountService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.Resource;
 
@@ -14,5 +16,11 @@ public class SysAccountController extends BaseController {
 
     @Resource
     private ISysAccountService sysAccountService;
+
+    @RequestMapping(value = "/account", method = RequestMethod.GET)
+    public String accountPage(){
+
+        return "";
+    }
 
 }
