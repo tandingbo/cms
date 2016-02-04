09d8ec28dc0c384faf878cd593b02f381f5cfa42
tandingbo@163.com <tandingbo>
2016-02-04 16:14:21 +0800

    tanbobo 1.0.0

---
+++ src/main/java/com/tanbobo/cms/base/common/constant/RSAConstant.java
@@ -0,0 +1,11 @@
+package com.tanbobo.cms.base.common.constant;
+
+/**
+ * 存放RSA密钥
+ */
+public class RSAConstant {
+    /** RSA-公钥 */
+    public static final String RSA_PUBLIC_KEY = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBALjqGYSjqr2BNObNoLreXmlYsGlNAWNKEzPRe3jRU1t2wepMt0ofq2it1C6f1xk0pjjPT1QWQCTS5995RS/WAasCAwEAAQ==";
+    /** RSA-私钥 */
+    public static final String RSA_PRIVATE_KEY = "MIIBVwIBADANBgkqhkiG9w0BAQEFAASCAUEwggE9AgEAAkEAuOoZhKOqvYE05s2gut5eaViwaU0BY0oTM9F7eNFTW3bB6ky3Sh+raK3ULp/XGTSmOM9PVBZAJNLn33lFL9YBqwIDAQABAkEAms8KbBxlrl/EvVH7tVbDreidUJsyhUCX0PMZLu34noHibBaEvFooKvlPHOL+D3e3rIoPlyxwSK5C8mr/Lw2N+QIhAOOVCwfyadsvuuvvtqKtnFug8S6p2s9bhHut/Yb6YZpnAiEA0AEgl04MOz3wZriwRvlhkLIz7nue1HUMWzJ6OnzR3B0CIQCNAeXJ0Lsh4ZrxYJ9KgEMmyrtk7Dz0yYF2mEFDhpM+9QIhAMXKfKJEDi97S5FyR/WFiDgerHE3TiY7Ez/ZNQ9OyAQlAiEAzy1bMijIzrPiNGLa8rBwGUdkmQb5kDBA6jGQ0C9QBsQ=";
+}
