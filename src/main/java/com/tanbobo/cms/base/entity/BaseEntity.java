3f26a542a93c6a8ce05d508a96dc74cea35e0c80
tandingbo@163.com <tandingbo>
2016-02-04 16:17:23 +0800

    tanbobo 1.0.0

---
+++ src/main/java/com/tanbobo/cms/base/entity/BaseEntity.java
@@ -0,0 +1,10 @@
+package com.tanbobo.cms.base.entity;
+
+import java.io.Serializable;
+
+/**
+ * 公共实体类
+ */
+public class BaseEntity<B extends BaseEntity<B>> implements Serializable {
+    private static final long serialVersionUID = -803715255303853661L;
+}
