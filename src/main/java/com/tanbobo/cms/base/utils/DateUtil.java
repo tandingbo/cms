e917bd701d8446527cb0683a5c9fa6caeec8ceb5
tandingbo@163.com <TAN19881218bo>
2016-02-14 16:02:11 +0800

    tanbobo 1.0.0

--- src/main/java/com/tanbobo/cms/base/utils/DateUtil.java
+++ src/main/java/com/tanbobo/cms/base/utils/DateUtil.java
@@ -7,17 +7,13 @@ import java.util.Date;
  * 日期工具类
  */
 public class DateUtil {
-    public DateUtil() {
-
-    }
-
     /**
      * 默认格式
      */
     private static final String DEFAULT_FORMAT = "yyyy-MM-dd HH:mm:ss";
-    private static final String FORMAT_DATE = "yyyy-MM-dd";
-    private static final String FORMAT_DATE_HOUR = "yyyy-MM-dd HH";
-    private static final String FORMAT_DATE_HOUR_MINUTE = "yyyy-MM-dd HH:mm";
+    public static final String FORMAT_DATE = "yyyy-MM-dd";
+    public static final String FORMAT_DATE_HOUR = "yyyy-MM-dd HH";
+    public static final String FORMAT_DATE_HOUR_MINUTE = "yyyy-MM-dd HH:mm";
 
     /**
      * 日期转字符串
